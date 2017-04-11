6c3d28bbc962b5c625df833e6c1e5932e0eb9c43
Timur Karpenok <nbvehbq@gmail.com>
2017-04-11 22:45:49 +0300

    compare ini

---
+++ __tests__/ini.test.js
@@ -0,0 +1,15 @@
+import compare from '../src/lib/compare';
+
+const path = name => `./__tests__/assets/${name}`;
+
+test('Compare json', () => {
+  expect(compare(path('one.ini'), path('two.ini')))
+  .toEqual(`{
+  + host: hexlet.ios
+  - host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+}`);
+});
--- src/lib/reader.js
+++ src/lib/reader.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (ext, file) => {
   const fullPath = path.isAbsolute(file)
@@ -10,6 +11,7 @@ export default (ext, file) => {
   switch (ext) {
     case 'json': return JSON.parse(buff);
     case 'yaml': return yaml.load(buff);
+    case 'ini': return ini.parse(buff);
     default: return {};
   }
 };
