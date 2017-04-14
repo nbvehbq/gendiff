2e905386c66cdd031df049a83fd4a2fbcb46a1ff
Timur Karpenok <nbvehbq@gmail.com>
2017-04-14 16:49:55 +0300

    continue

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -4,14 +4,39 @@ const buildPath = name => `./__tests__/__fixtures__/${name}`;
 
 const res =
 `{
-  + host: hexlet.ios
-  - host: hexlet.io
+    host: hexlet.io
   + timeout: 20
   - timeout: 50
   - proxy: 123.234.53.22
   + verbose: true
 }`;
 
+const rescomplex =
+`{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      - setting6: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
+}`;
 describe('Compare tests...', () => {
   it('Compare flat json', () => {
     expect(compare(buildPath('one.json'), buildPath('two.json')))
@@ -26,6 +51,11 @@ describe('Compare tests...', () => {
   it('Compare flat ini', () => {
     expect(compare(buildPath('one.ini'), buildPath('two.ini')))
     .toEqual(res);
+  });
+  
+  it('Compare complex json', () => {
+    expect(compare(buildPath('one_complex.json'), buildPath('two_complex.json')))
+    .toEqual(rescomplex);
   });
 });
 
--- src/compare.js
+++ src/compare.js
@@ -1,29 +1,48 @@
 import _ from 'lodash';
 import read from './reader';
 
-const fillAst = (firstBuff, secondBuff) => {
+const isObject = item => (typeof item === 'object');
+
+const fillAst = (firstBuff, secondBuff, deep = 0) => {
   const keys = _.union(Object.keys(firstBuff), Object.keys(secondBuff));
 
   const res = keys.reduce((acc, key) => {
-    if (!firstBuff[key]) {
-      return [...acc, { key, value: secondBuff[key], status: '+' }];
+    const left = firstBuff[key];
+	const right = secondBuff[key];
+
+    if (!left) {
+	  const value = isObject(right) ? fillAst(right, right, deep + 1) : right;
+      return [...acc, { key, value, status: '+', deep }];
     }
-    if (!secondBuff[key]) {
-      return [...acc, { key, value: firstBuff[key], status: '-' }];
+    if (!right) {
+	  const value = isObject(left) ? fillAst(left, left, deep + 1) : left;
+      return [...acc, { key, value, status: '-', deep }];
     }
-    if (firstBuff[key] === secondBuff[key]) {
-      return [...acc, { key, value: firstBuff[key], status: ' ' }];
+    if (left === right) {
+	  const value = isObject(left) ? fillAst(left, left, deep + 1) : left;
+      return [...acc, { key, value, status: ' ', deep }];
     }
-    const add = [...acc, { key, value: secondBuff[key], status: '+' }];
-    return [...add, { key, value: firstBuff[key], status: '-' }];
+	if (isObject(right) && isObject(left)) {
+	  const value = fillAst(left, right, deep + 1);
+	  return [...acc, { key, value, status: ' ', deep }];
+	}
+	let value = isObject(right) ? fillAst(right, right, deep + 1) : right;
+    const add = [...acc, { key, value, status: '+', deep }];
+	value = isObject(left) ? fillAst(left, left, deep + 1) : left;
+    return [...add, { key, value, status: '-', deep }];
   }, []);
 
   return res;
 };
 
 const printDiff = (ast) => {
-  const res = ast.map(item => `${item.status} ${item.key}: ${item.value}`);
-  return `{\n  ${res.join('\n  ')}\n}`;
+  const isComplex = item => (item instanceof Array);
+  const res = ast.map((item) => {
+    const stub = ' '.repeat(item.deep);
+    return `${stub}${item.status} ${item.key}: ${isComplex(item.value) ? printDiff(item.value) : item.value}\n${stub}`;
+  });
+
+  return `{\n${res.join('')}}`;
 };
 
 export default (before, after) => printDiff(fillAst(read(before), read(after)));
